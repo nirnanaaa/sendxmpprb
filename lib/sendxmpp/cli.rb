23b47e2037565f70aeb3ee675e7d4194f8485ac7
nirnanaaa <mosny@zyg.li>
2014-05-14 14:05:35 +0200

    updated cli for multi user

--- lib/sendxmpp/cli.rb
+++ lib/sendxmpp/cli.rb
@@ -22,6 +22,7 @@ module Sendxmpp
     class_option :tls_ca, default: "", type: :string, required: false
     class_option :resource, default: "Sendxmpp", type: :string, required: false
     class_option :password, default: "", type: :string, required: false
+    class_option :message, default: "", type: :string, aliases: '-m', required: false
     class_option :jid, default: "", type: :string, required: false
     class_option :config, default: "#{ENV['HOME']}/.sendxmpprbrc", type: :string, aliases: "-c", required: false
     class_option :logfile, default: nil, aliases: '-l', required: false
@@ -34,21 +35,20 @@ module Sendxmpp
     # args - Arguments, passed to Thor
     #
     # Raises IniFile::Error on invalid configuration
+    # Raises ArgumentError if the main hash key was not found
     def initialize(*args)
       super
       if File.exists?(options[:config])
         conf = IniFile.load(options[:config])["sendxmpp"]
+        if conf.nil? || conf.empty?
+          raise ArgumentError, "No [sendxmpp] section in ini file found!"
+        end
         local_conf = options.dup
+        local_conf.delete_if{|k,v|v.nil?||(v.kind_of?(String) && v.empty?)}
         conf.merge!(local_conf)
         update_config(conf)
       end
-      if config.logfile
-        @log = Logger.new(config.logfile)
-      else
-        @log = Logger.new(STDOUT)
-      end
-      log.level = config.loglevel.to_i || 2
-      log.debug("finished loading configuration.")
+      Log.logger.debug("finished loading configuration.")
       $stdout.sync = true
     end
 
@@ -64,23 +64,56 @@ module Sendxmpp
     #   ...
     #
     # Returns 0 or 1 exit codes
-    def user(jids)
-      log.debug("Received call for user method")
+    def user(*jids)
+      Log.logger.debug("Received call for user method")
+      unless jids.kind_of?(Array)
+        Log.logger.error("Throwing ArgumentError because Jids is not an array.")
+        raise ArgumentError, "Jids needs to be an Array got #{jids.class}"
+      end
+
+      if options.message.empty?
+        Log.logger.error("No message to send. Exiting.")
+        Log.logger.error("See https://github.com/nirnanaaa/sendxmpprb/wiki/Sending-messages for available message formats.")
+        exit 1
+      end
+
+      Message.batch do
+        jids.each do |jid|
+          Message.message_to_user(jid)
+        end
+      end
     end
 
     desc "chat [CHAT1],[CHAT2],...", "send a message to multiple multi user chats"
     # Public: Send a message to a single/multiple multi user chatrooms.
     #         Messages will be sent to each chat seperately
     #
-    # rooms - Array of MUC(Multi user chat) jids.
+    # jids - Array of MUC(Multi user chat) jids.
     #
     # Examples
     #   chat(["edv@conference.jabber.org"])
     #   chat(["edv@conference.jabber.org", "staff@conference.jabber.org"])
     #
     # Returns 0 or 1 exit codes
-    def chat(rooms)
-      log.debug("Received call for chat method")
+    def chat(*jids)
+      Log.logger.debug("Received call for chat method")
+      unless jids.kind_of?(Array)
+        Log.logger.error("Throwing ArgumentError because Jids is not an array.")
+        raise ArgumentError, "Jids needs to be an Array got #{jids.class}"
+      end
+
+      if options.message.empty?
+        Log.logger.error("No message to send. Exiting.")
+        Log.logger.error("See https://github.com/nirnanaaa/sendxmpprb/wiki/Sending-messages for available message formats.")
+        exit 1
+      end
+
+      Message.batch do
+        jids.each do |jid|
+          Message.message_to_room(jid)
+        end
+      end
+
     end
 
   end
